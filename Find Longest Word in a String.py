06b0e9142c061feb38516f1e3aef1b14678c33de
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-10 17:06:26 +0200

    add-Find Longest Word in a Stringcount=0

--- Find Longest Word in a String.py
+++ Find Longest Word in a String.py
@@ -1,9 +1,21 @@
+'''
 def longest_word_in(sentence):
-    length=''
+    longest=''
     longest_word= sentence.split(' ')
     for word in longest_word:
-        if len(word)>len(length):
-            length = word
-        return length
+        if len(word)>len(longest):
+            longest = word
+        return longest
+
+print(longest_word_in('Hallooooooooo i am anas'))
+'''
+def longest_word_in(sentence):
+    count = 0
+    longest_word= sentence.split(' ')
+    for word in longest_word:
+        if len(word)>count:
+            count = len(word) 
+            longest = word
+        return longest
 
 print(longest_word_in('Hallooooooooo i am anas'))
